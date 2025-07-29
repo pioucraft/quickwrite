d58ad2d966cff683685e49afe31be357da58b0ac
Nathan <hello@gougoule.ch>
2025-07-29 20:19:56 +0200

    Implement some logic for some things

---
+++ lib/strings.c
@@ -0,0 +1,6 @@
+#include <strings.h>
+
+char** split_string(const char* str, const char* delimiter, int* count) {
+    
+}
+
---
+++ lib/strings.h
@@ -0,0 +1,7 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+
+char** split_string(const char* str, const char* delimiter, int* count)
+
+#endif
+
